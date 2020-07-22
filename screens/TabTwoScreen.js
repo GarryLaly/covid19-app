f70b799b583a3345a473df83cb2e4b5f48acdde2
Gigih Hadi P <hadigigih@gmail.com>
2020-07-22 11:34:49 +0700

    [FIX] bisa mendapatkan data indonesia & global

--- screens/TabTwoScreen.js
+++ screens/TabTwoScreen.js
@@ -38,47 +38,29 @@ const Tulis = styled.Text``;
 const TabTwoScreen = () => {
   const [numbers, setNumbers] = useState({});
   const [toggle, setToggle] = useState({status:false, nation:'Indonesia'});
-  const [callFunction, setCallfunction] = useState(false);
-
-  // var {indonesia} = getData();
-  // setStats(data);
 
   useEffect( ()=>{
-    const fetchCovid=async()=> {
-        // var {data} = await api.get();
-        // confirm=data.reduce(function(sum, d){
-        //     return sum + d.attributes.Confirmed
-        // },0)
-        // recovered=data.reduce(function(sum, d){
-        //     return sum + d.attributes.Recovered
-        // },0)
-        // active=data.reduce(function(sum, d){ 
-        //     return sum + d.attributes.Active
-        // },0)
-        // death=data.reduce(function(sum, d){ 
-        //     return sum + d.attributes.Deaths
-        // },0)
-        // total_glob={confirm:confirm,recovered:recovered,active:active,death:death}
-        const {data} = await api.get((toggle.value?'':'indonesia'));
+    const fetchCovid=async(is_global)=> {
+        const {data} = await api.get((is_global?'':'indonesia'));
         confirm=data.reduce(function(sum, d){
-            return sum + (toggle.value?d.attributes.Confirmed:parseInt(d.positif.replace(',','')))
+            return sum + (is_global?d.attributes.Confirmed:parseInt(d.positif.replace(',','')))
         },0)
         recovered=data.reduce(function(sum, d){
-            return sum + (toggle.value?d.attributes.Recovered:parseInt(d.sembuh.replace(',','')))
+            return sum + (is_global?d.attributes.Recovered:parseInt(d.sembuh.replace(',','')))
         },0)
         active=data.reduce(function(sum, d){ 
-            return sum + (toggle.value?d.attributes.Active:parseInt(d.dirawat.replace(',','')))
+            return sum + (is_global?d.attributes.Active:parseInt(d.dirawat.replace(',','')))
         },0)
         death=data.reduce(function(sum, d){ 
-            return sum + (toggle.value?d.attributes.Deaths:parseInt(d.meninggal.replace(',','')))
+            return sum + (is_global?d.attributes.Deaths:parseInt(d.meninggal.replace(',','')))
         },0)
-        // total_idn={confirm:confirm,recovered:recovered,active:active,death:death}
-        // total={indonesia:total_idn, global:total_glob}
+
         total = {confirm:confirm,recovered:recovered,active:active,death:death}
         setNumbers(total);
     };
-    fetchCovid();
-  },[toggle.value]);
+
+    fetchCovid(toggle.status);
+  },[toggle]);
 
   return (
     <Container>
@@ -95,19 +77,19 @@ const TabTwoScreen = () => {
 
       <Row>
         <Col size={1}>
-          <Tulis>{JSON.stringify(numbers)}</Tulis>
-          {/* <BoxInfo data={toggle.value?data.indonesia.confirm:data.global.confirm} boxColor="#DC8700" title="Confirm"></BoxInfo> */}
+          {/* <Tulis>{numbers.confirm}</Tulis> */}
+          <BoxInfo data={numbers.confirm} boxColor="#DC8700" title="Confirm"></BoxInfo>
         </Col>
         <Col size={1}>
-          {/* <BoxInfo data={toggle.value?data.indonesia.death:data.global.death} boxColor="#f00" title="Death"></BoxInfo> */}
+          <BoxInfo data={numbers.death} boxColor="#f00" title="Death"></BoxInfo>
         </Col>
       </Row>
       <Row>
         <Col size={1}>
-          {/* <BoxInfo data={toggle.value?data.indonesia.recovered:data.global.recovered} boxColor="#40BA1C" title="Recovered"></BoxInfo> */}
+          <BoxInfo data={numbers.recovered} boxColor="#40BA1C" title="Recovered"></BoxInfo>
         </Col>
         <Col size={1}>
-          {/* <BoxInfo data={toggle.value?data.indonesia.active:data.global.active} boxColor="#1D9FC4" title="Active"></BoxInfo> */}
+          <BoxInfo data={numbers.active} boxColor="#1D9FC4" title="Active"></BoxInfo>
         </Col>
       </Row>
     </Container>
